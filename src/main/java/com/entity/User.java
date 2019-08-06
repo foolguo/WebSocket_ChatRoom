91e5098d74487988928c92d4da60ccaf818791a0
foolguo <1103423563@qq.com>
2019-08-06 11:36:39 +0800

    相约HI聊

---
+++ src/main/java/com/entity/User.java
@@ -0,0 +1,15 @@
+package com.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: yuisama
+ * @Date: 2019-07-30 21:22
+ * @Description:
+ */
+@Data
+public class User {
+    private Integer id;
+    private String userName;
+    private String password;
+}
